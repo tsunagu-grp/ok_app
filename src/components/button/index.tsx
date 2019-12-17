1e26174031bf5ead0c61b50ae9058d47948d6dc6
skawadu <skawatsu@geek.co.jp>
2019-12-17 18:40:37 +0900

    Update Acquire "button coordinates" on Redux ・content(入力)データをonClickイベントでReduxを介して投稿するテスト

--- src/components/button/index.tsx
+++ src/components/button/index.tsx
@@ -1,10 +1,29 @@
 import React from 'react';
+import {RootState} from '../../store/rootReducer';
+import {useDispatch, useSelector} from 'react-redux';
+import {updateTalk} from '../../containers/talkSlice';
 import {Button} from 'antd';
 
 const SubmitButton: React.FC = () => {
+  const dispatch = useDispatch();
+  const talk = useSelector((state: RootState) => state.talk.talk.data);
+  const lastTalkID = talk[talk.length - 1].id;
+  const content = useSelector((state: RootState) => state.talk.content.content);
+  const handleClick = () => {
+    const data = [
+      ...talk,
+      {
+        id: lastTalkID + 1,
+        content: content,
+        timestamp: new Date(),
+        user_id: 1,
+      },
+    ];
+    dispatch(updateTalk(data));
+  };
   return (
     <div>
-      <Button type="primary" htmlType="submit">
+      <Button type="primary" htmlType="submit" onClick={handleClick}>
         送信する
       </Button>
     </div>
